e710bb0aa20d31c1208ae7487b4ed2d54a4cc68f
Alexandro02 <alexx1098@outlook.es>
2025-05-09 11:07:49 -0600

    connected backend for navTop and Footer - test

---
+++ src/utils/queries/fetchNavTop.ts
@@ -0,0 +1,9 @@
+import { fetchProtected } from "@/src/utils/api/fetchProtected";
+import type { TopBar } from "@/src/utils/types";
+
+const url = `${import.meta.env.WP_REST_ENDPOINT_ACF}/pages/97`;
+
+export async function fetchNavTop(): Promise<TopBar> {
+  const { acf } = await fetchProtected(url);
+  return acf;
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+export type TopBar = {
+  phone: string;
+  email: string;
+  schedule: string;
+  fb_link: string;
+  in_link: string;
+}
