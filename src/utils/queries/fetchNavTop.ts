1cf049b53d09daf33c92a0252d6490699f2532d0
Alexandro02 <alexx1098@outlook.es>
2025-05-13 00:41:27 -0600

    file structure refactor

--- src/utils/queries/fetchNavTop.ts
+++ src/utils/queries/fetchNavTop.ts
@@ -3,7 +3,7 @@ import type { TopBar } from "@/src/utils/types";
 
 const url = `${import.meta.env.VITE_WP_REST_ENDPOINT_ACF}/pages/97`;
 
-export async function fetchNavTop(): Promise<TopBar> {
+export async function fetchTopSection(): Promise<TopBar> {
   const { acf } = await fetchProtected(url);
   return acf;
 }
