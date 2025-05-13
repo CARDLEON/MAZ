3cf9851d743118c6afd690fd100b37e94c317741
Alexandro02 <alexx1098@outlook.es>
2025-05-13 12:14:48 -0600

    changed from static to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://maz.sandboxcardleon.com",
-  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
