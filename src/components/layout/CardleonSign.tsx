79d7aad34be1bc31e1ee8f427192d28ba1982a1b
Alexandro02 <alexx1098@outlook.es>
2025-10-08 14:19:51 -0600

    feat: new shi happenin
    enchanced footer layout
    changed email contact from top bar
    changed whatsapp contact number
    updated example dev variables

--- src/components/layout/CardleonSign.tsx
+++ src/components/layout/CardleonSign.tsx
@@ -1,6 +1,6 @@
 export default function CardleonSign() {
   return (
-    <footer className="w-full bg-[#5B348B] text-white border-t border-[#24408d]">
+    <footer className="w-full bg-[#202020] text-white">
       <div className="max-w-full mx-auto px-6 sm:px-12 md:px-20 lg:px-[8rem] py-4 flex flex-col sm:flex-row justify-between items-center gap-4 text-sm sm:text-base">
         {/* Lado izquierdo */}
         <div className="text-center sm:text-left font-semibold">
@@ -8,17 +8,19 @@ export default function CardleonSign() {
         </div>
 
         {/* Lado derecho */}
-        <div className="flex items-center justify-center sm:justify-end gap-2 text-center sm:text-right">
+        <div className="flex items-center justify-center sm:justify-end gap-4 text-center sm:text-right">
           <span className="font-medium">Powered by</span>
           <a
-            href="https://cardleon.com"
+            href="https://wa.me/+528134209552?text=Quisiera%20informes%20para%20paginas%20web"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="Sitio web de CARDLEON"
+            aria-label="contact-whatsapp"
           >
             <img
-              src="/CardleonLogo.png"
-              alt="Agencia de marketing CARDLEON enfocados en el rubro industrial."
+              width={100}
+              height={100}
+              src="/ThornLogo.png"
+              alt="Logo de Thorn Solutions"
               className="h-10 sm:h-10 w-auto"
             />
           </a>
